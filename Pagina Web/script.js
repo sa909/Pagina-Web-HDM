21263b57f98256fc74e9818a3697f74413a0337e
sa909 <72840175+sa909@users.noreply.github.com>
2026-08-26 09:24:09 -0300

    Update script.js

--- Pagina Web/script.js
+++ Pagina Web/script.js
@@ -1,5 +1,35 @@
-const boton = document.getElementById("boton");
+function sumar() {
+    let numero1 = Number(document.getElementById("numero1").value);
+    let numero2 = Number(document.getElementById("numero2").value);
 
-boton.addEventListener("click", function() {
-    alert("¡Gracias por visitar nuestra revista digital!");
-});
+    let resultado = numero1 + numero2;
+
+    document.getElementById("resultado").textContent = "Resultado: " + resultado;
+}
+
+function restar() {
+    let numero1 = Number(document.getElementById("numero1").value);
+    let numero2 = Number(document.getElementById("numero2").value);
+
+    let resultado = numero1 - numero2;
+
+    document.getElementById("resultado").textContent = "Resultado: " + resultado;
+}
+
+function multiplicar() {
+    let numero1 = Number(document.getElementById("numero1").value);
+    let numero2 = Number(document.getElementById("numero2").value);
+
+    let resultado = numero1 * numero2;
+
+    document.getElementById("resultado").textContent = "Resultado: " + resultado;
+}
+
+function dividir() {
+    let numero1 = Number(document.getElementById("numero1").value);
+    let numero2 = Number(document.getElementById("numero2").value);
+
+    let resultado = numero1 / numero2;
+
+    document.getElementById("resultado").textContent = "Resultado: " + resultado;
+}
